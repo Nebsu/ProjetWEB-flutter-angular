1cf0acfc9c4fdf3db9975581ea1be281112d7c37
Nicolas Theau <theaun91@gmail.com>
2024-11-26 12:22:17 +0100

    Ajout du trie

--- ma_todo_flutter/lib/components/todo_list.dart
+++ ma_todo_flutter/lib/components/todo_list.dart
@@ -1,9 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../services/todo_service.dart';
 import 'todo_item.dart';
 
 class TodoList extends StatelessWidget {
   const TodoList({super.key});
+
+  List<Map<String, dynamic>> _sortTodos(List<Map<String, dynamic>> todos) {
+    todos.sort((a, b) {
+      // Trier par état de complétion (non complétés en premier)
+      if (a['completed'] != b['completed']) {
+        return a['completed'] ? 1 : -1;
+      }
+
+      // Trier par dueDate (ceux avec dueDate en premier)
+      if (a['dueDate'] != null && b['dueDate'] == null) {
+        return -1;
+      }
+      if (a['dueDate'] == null && b['dueDate'] != null) {
+        return 1;
+      }
+
+      // Trier par dueDate ou creationDate
+      DateTime dateA = a['dueDate'] != null ? (a['dueDate'] as Timestamp).toDate() : (a['creationDate'] as Timestamp).toDate();
+      DateTime dateB = b['dueDate'] != null ? (b['dueDate'] as Timestamp).toDate() : (b['creationDate'] as Timestamp).toDate();
+      return dateA.compareTo(dateB);
+    });
+    return todos;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,10 +41,11 @@ class TodoList extends StatelessWidget {
           return Center(child: Text('Erreur: ${snapshot.error}'));
         }
         final todos = snapshot.data ?? [];
+        final sortedTodos = _sortTodos(todos);
         return ListView.builder(
-          itemCount: todos.length,
+          itemCount: sortedTodos.length,
           itemBuilder: (context, index) {
-            return TodoItem(todo: todos[index]);
+            return TodoItem(todo: sortedTodos[index]);
           },
         );
       },
