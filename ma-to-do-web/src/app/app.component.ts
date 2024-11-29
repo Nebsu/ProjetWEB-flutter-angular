54239d7a36a0bc8b79ceef401b9e6beedf526776
Yanis Rozier <yanis.rozier@orange.fr>
2024-11-29 15:07:07 +0100

    night mode

--- ma-to-do-web/src/app/app.component.ts
+++ ma-to-do-web/src/app/app.component.ts
@@ -10,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ma-to-do-web';
+
+  toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+  }
 }
